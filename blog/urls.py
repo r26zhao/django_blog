385b0fb3ba0a62127ad790c30a73c028bbd2a99a
Aaron Zhao <rudy710@qq.com>
2017-06-21 16:38:58 +0800

    update blog.urls

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^post/(?P<pk>[0-9]+)/$', views.detail, name='detail'),
     url(r'^category/(?P<slug>[a-zA-Z0-9_\-\u4e00-\u9fa5]+)/$', views.category, name='category'),
+    url(r'^tag/(?P<slug>[a-zA-Z0-9_\-\u4e00-\u9fa5]+)/$', views.tag, name='tag'),
     url(r'^search/$', views.search, name='search'),
 ]
