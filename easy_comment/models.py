5299689b3785464e23c4964f6663632ddd88a425
Aaron Zhao <rudy710@qq.com>
2017-07-20 20:00:21 +0800

    增加点赞功能

--- easy_comment/models.py
+++ easy_comment/models.py
@@ -1,15 +1,16 @@
 from django.db import models
-from blog.models import User, Post
-from mptt.models import MPTTModel, TreeForeignKey
+from django.conf import settings
+from mptt.models import TreeForeignKey, MPTTModel
 from ckeditor_uploader.fields import RichTextUploadingField
+
 # Create your models here.
 
 class Comment(MPTTModel):
-    user = models.ForeignKey(User, blank=True, null=True, related_name='comments')
-    user_name = models.CharField(blank=True, max_length=50, null=True)
-    post = models.ForeignKey(Post, verbose_name='文章')
-    parent = TreeForeignKey('self', blank=True, null=True, related_name='children', verbose_name='父级评论')
-    content = RichTextUploadingField(verbose_name='评论', config_name='comment',)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True,)
+    user_name = models.CharField(max_length=50, blank=True, null=True)
+    post = models.ForeignKey(settings.COMMENT_ENTRY_MODEL, verbose_name='文章')
+    parent = TreeForeignKey('self', blank=True, null=True, verbose_name='父级评论')
+    content = RichTextUploadingField(verbose_name='评论', config_name='comment')
     submit_date = models.DateTimeField(auto_now_add=True, verbose_name='提交时间')
 
     class MPTTMeta:
@@ -17,5 +18,19 @@ class Comment(MPTTModel):
 
     def __str__(self):
         if self.parent is not None:
-            return '%s --> %s' % (self.user.username, self.parent.user.username)
-        return 'post_%s___%s' % (str(self.post.id), self.user.username)
+            return '%s 回复 %s' % (self.user_name, self.parent.user_name)
+        return '%s 评论文章 post_%s' % (self.user_name, str(self.post.id))
+
+class Like(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    comment = models.ForeignKey(Comment)
+    created_time = models.DateTimeField(auto_now_add=True)
+    status = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = '点赞'
+        verbose_name_plural = verbose_name
+    def __str__(self):
+        if self.status:
+            return '%s 赞了 %s的评论' % (self.user.username, self.comment.user_name)
+        return '%s 踩了 %s的评论' % (self.user.username, self.comment.user_name)
