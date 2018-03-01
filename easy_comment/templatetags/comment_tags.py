b6d6635952fc32afc086926106656415264ff0dd
Aaron Zhao <rudy710@qq.com>
2018-03-01 17:01:59 +0800

    评论功能demo

--- easy_comment/templatetags/comment_tags.py
+++ easy_comment/templatetags/comment_tags.py
@@ -4,6 +4,8 @@ from ..models import Like
 from django.conf import settings
 from django.apps import apps
 from django.db.models.aggregates import Count
+from django.core.cache import cache
+from django.template.loader import render_to_string
 
 register = template.Library()
 
@@ -48,3 +50,20 @@ def get_comment_rank(num=5):
     Post = apps.get_model(*app_model)
     post_list = Post.objects.annotate(comment_num=Count('comment')).order_by('-comment_num')
     return post_list[:num]
+
+@register.simple_tag
+def generate_comment_list(post):
+    key1 = 'post_{}_comments'.format(post.id)
+    html = cache.get(key1, None)
+    if not html:
+        html = ''
+        comments = post.comment_set.all().order_by('-submit_date')
+        for comment in comments:
+            key = 'comment_{}_html'.format(comment.id)
+            comment_html = cache.get(key, None)
+            if not comment_html:
+                comment_html = render_to_string('easy_comment/comment_entry.html', context={'comment': comment})
+                cache.set(key, comment_html, timeout=300)
+            html += comment_html
+        cache.set(key1, html, timeout=300)
+    return html
