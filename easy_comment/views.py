2e6e9a1cc4e4d382385d32b9344ab176594e8744
Aaron Zhao <rudy710@qq.com>
2018-03-04 08:09:27 +0800

    ajax not solved yet

--- easy_comment/views.py
+++ easy_comment/views.py
@@ -2,6 +2,8 @@ from .forms import CommentForm
 from .models import Comment, Like
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
+from django.template.loader import render_to_string
+from django.core.cache import cache
 from . import handlers
 
 
@@ -10,15 +12,22 @@ from . import handlers
 @require_POST
 def submit_comment(request):
     form = CommentForm(data=request.POST)
-    # print(request.POST)
     if form.is_valid():
         # print('success')
         new_comment = form.save(commit=False)
         new_comment.user = request.user
         new_comment.user_name = request.user.username
         new_comment.save()
-        location = "#c" + str(new_comment.id)
-        return JsonResponse({'msg': 'success!', 'new_comment': location})
+        key = 'comment_{}_html'.format(new_comment.id)
+        comment_html = render_to_string('easy_comment/comment_entry.html', context={'comment': new_comment})
+        cache.set(key, comment_html, timeout=300)
+        post = new_comment.post
+        key1 = 'post_{}_comments'.format(post.id)
+        comment_list_html = cache.get(key1, None)
+        if comment_list_html:
+            comment_list_html = comment_html + comment_list_html
+            cache.set(key1, comment_list_html, timeout=300)
+        return JsonResponse({'msg': 'success!', 'html': comment_html})
     return JsonResponse({'msg': '评论出错!'})
 
 
