5e98b3dcd0d5921ae337226da8249a063628ccb2
Aaron Zhao <rudy710@qq.com>
2018-03-15 00:10:43 +0800

    增加点赞model

--- easy_comment/models.py
+++ easy_comment/models.py
@@ -4,8 +4,24 @@ from mptt.models import TreeForeignKey, MPTTModel
 from ckeditor_uploader.fields import RichTextUploadingField
 from django.core.cache import cache
 from django.template.loader import render_to_string
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
 
 # Create your models here.
+
+class Favour(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='favours')
+    content_type = models.ForeignKey(ContentType)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-date_created',)
+
+    def __str__(self):
+        return "{} 喜欢 {}_{}".format(self.user.username, self.content_object._meta.model_name, self.content_object.id)
+
 
 class Comment(MPTTModel):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True,)
@@ -14,8 +30,7 @@ class Comment(MPTTModel):
     parent = TreeForeignKey('self', blank=True, null=True, verbose_name='父级评论',)
     content = RichTextUploadingField(verbose_name='评论', config_name='comment')
     submit_date = models.DateTimeField(auto_now_add=True, verbose_name='提交时间')
-    users_like = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True, related_name='comments_liked')
-    users_dislike = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True, related_name='comments_disliked')
+    favours = GenericRelation(Favour)
 
     class MPTTMeta:
         order_insertion_by = ['submit_date']
@@ -32,13 +47,3 @@ class Comment(MPTTModel):
             html = render_to_string('easy_comment/comment_entry.html', context={'comment': self})
             cache.set(key, html, timeout=300)
         return html
-
-    def likes_count(self, update=False):
-        key = 'comment_{}_likes'.format(self.id)
-        result = cache.get(key)
-        if update or result is None:
-            result = {}
-            result['likes'] = self.users_like.all().count()
-            result['dislikes'] = -self.users_dislike.all().count()
-            cache.set(key, result, timeout=300)
-        return result
